1e0d98e83fc2be77f05b056de47c41a43c9c6ee7
Soelin Tun <soelintun700@gmail.com>
2017-07-30 03:03:09 -0700

    highcharts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { Es6SolutionsModule } from './es6-solutions/es6-solutions.module';
-
 import { DataVisualizationModule } from './data-visualization/data-visualization.module';
 import { ControlsModule } from './controls/controls.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ChartModule} from 'angular2-highcharts';
+import {HighchartsStatic} from 'angular2-highcharts/dist/HighchartsService';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 
-
+export function highchartsFactory() {
+  return require('highcharts');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +22,13 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
     AppRoutingModule,
     ControlsModule,
     DataVisualizationModule,
-    Es6SolutionsModule
+    Es6SolutionsModule,
+    ChartModule
+   
   ],
-  providers: [],
+  providers: [
+    {provide: HighchartsStatic, useFactory: highchartsFactory}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
